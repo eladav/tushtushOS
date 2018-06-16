6629de2475d634fcb994ca06cc4d61a988504ce0
Live session user <eladav86@gmail.com>
2018-06-16 13:25:15 +0000

    move cursor

--- kmain.c
+++ kmain.c
@@ -1,3 +1,26 @@
+        #include "io.h"
+
+    /* The I/O ports */
+    #define FB_COMMAND_PORT         0x3D4
+    #define FB_DATA_PORT            0x3D5
+
+    /* The I/O port commands */
+    #define FB_HIGH_BYTE_COMMAND    14
+    #define FB_LOW_BYTE_COMMAND     15
+
+    /** fb_move_cursor:
+     *  Moves the cursor of the framebuffer to the given position
+     *
+     *  @param pos The new position of the cursor
+     */
+    void fb_move_cursor(unsigned short pos)
+    {
+        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    pos & 0x00FF);
+    }
+    
     /** fb_write_cell:
      *  Writes a character with the given foreground and background to position i
      *  in the framebuffer.
@@ -32,5 +55,7 @@ int kmain() {
     fb_write_cell(14, 'h', FB_GREEN, FB_DARK_GREY);
     fb_write_cell(16, 'O', FB_GREEN, FB_DARK_GREY);
     fb_write_cell(18, 'S', FB_GREEN, FB_DARK_GREY);
+
+    fb_move_cursor(10);
     return 0;
 }
