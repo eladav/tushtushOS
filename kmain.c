a48f14607cfacc60a67c860354cc4d14a694d69a
Live session user <eladav86@gmail.com>
2018-06-16 12:56:19 +0000

    print to frame buffer

--- kmain.c
+++ kmain.c
@@ -1,3 +1,36 @@
+    /** fb_write_cell:
+     *  Writes a character with the given foreground and background to position i
+     *  in the framebuffer.
+     *
+     *  @param i  The location in the framebuffer
+     *  @param c  The character
+     *  @param fg The foreground color
+     *  @param bg The background color
+     */
+    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+    {
+        char *fb = (char *) 0x000B8000;
+        fb[i] = c;
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+    }
+
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+    #define FB_BLACK     0
+    
 int kmain() {
+    for (int i = 0; i < 4000; i = i + 2) {
+        fb_write_cell(i, ' ', FB_BLACK, FB_DARK_GREY);
+    }
+    fb_write_cell(0, 't', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(2, 'u', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(4, 's', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(6, 'h', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(8, 't', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(10, 'u', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(12, 's', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(14, 'h', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(16, 'O', FB_GREEN, FB_DARK_GREY);
+    fb_write_cell(18, 'S', FB_GREEN, FB_DARK_GREY);
     return 0;
 }
